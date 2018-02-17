69e2749e2764f33cf819bb9d46e74e7590423b38
Mateo Escobedo <mat.esc@outlook.com>
2018-02-17 16:57:09 -0500

    Initial commit

--- src/main/java/frc/team5190/feeder/Constants.java
+++ src/main/java/frc/team5190/feeder/Constants.java
@@ -2,8 +2,8 @@ package frc.team5190.feeder;
 
 public class Constants {
     public static final double TIME_DELTA = 0.02;
-    public static final double MAX_VELOCITY = 6.5;
-    public static final double MAX_ACCELERATION = 4.0;
+    public static final double MAX_VELOCITY = 7.0;
+    public static final double MAX_ACCELERATION = 5.0;
     public static final double MAX_JERK = 60.0;
-    public static final double WHEEL_WIDTH = 27;          // inches
+    public static final double WHEEL_WIDTH = 28;          // inches
 }
