8cdeb06fe8bb74ea80db49e2083f2e3534384f35
Ryan Segerstrom <13050203+ThePrimedTNT@users.noreply.github.com>
2018-03-29 11:03:28 -0400

    should work

--- src/main/kotlin/FileWriter.kt
+++ src/main/kotlin/FileWriter.kt
@@ -1,4 +1,4 @@
-import frc.team5190.feeder.Constants
+import frc.team5190.feeder.PathFeeder
 import jaci.pathfinder.*
 import jaci.pathfinder.modifiers.TankModifier
 import java.io.*
@@ -14,42 +14,12 @@ object FileWriter {
         for (folderpath in folderpaths) {
             for (j in 1 until folderpath.size) {
                 try {
-                    val waypoints = ArrayList<Waypoint>()
-
-                    val reader = InputStreamReader(ClassLoader.getSystemResourceAsStream(folderpath[0] + "/" + folderpath[j] + ".bot"))
-                    val bufferedReader = BufferedReader(reader)
-                    val trajectories = arrayOfNulls<Trajectory>(2)
-
-
-                    var line = bufferedReader.readLine()
-                    while (line != null) {
-
-                        val tokenize = line.split(",".toRegex()).dropLastWhile({ it.isEmpty() }).toTypedArray()
-                        if (tokenize.size == 3) {
-                            waypoints.add(Waypoint(
-                                    (tokenize[0]).toDouble(),
-                                    (tokenize[1]).toDouble(),
-                                    (Pathfinder.d2r((tokenize[2].toDouble())))))
-                        }
-                        line = bufferedReader.readLine()
-                    }
-
-                    val config = Trajectory.Config(
-                            Trajectory.FitMethod.HERMITE_QUINTIC, Trajectory.Config.SAMPLES_HIGH,
-                            Constants.TIME_DELTA, 7.5,
-                            4.0, Constants.MAX_JERK)
-                    val trajectory = Pathfinder.generate(waypoints.toTypedArray(), config)
-                    val modifier = TankModifier(trajectory)
-                    modifier.modify(Constants.WHEEL_WIDTH / 12.0)
-
-                    trajectories[0] = modifier.leftTrajectory
-                    trajectories[1] = modifier.rightTrajectory
+                    val trajectories = PathFeeder.generatePath(folderpath[0], folderpath[j])!!
 
                     println("Processing " + folderpath[0] + "/" + folderpath[j])
 
                     Pathfinder.writeToCSV(File("folders/" + folderpath[0] + "/" + folderpath[j] + " Left Detailed.csv"), trajectories[0])
                     Pathfinder.writeToCSV(File("folders/" + folderpath[0] + "/" + folderpath[j] + " Right Detailed.csv"), trajectories[1])
-
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
--- src/main/kotlin/frc/team5190/feeder/PathFeeder.kt
+++ src/main/kotlin/frc/team5190/feeder/PathFeeder.kt
@@ -32,57 +32,11 @@ object PathFeeder {
             val folderName = request.folderName
             val fileName = request.fileName
 
-            val filePath = "$folderName/$fileName.xml"
-
             display.background = Color.YELLOW
             println("Received request: $folderName/$fileName")
 
-            val doc = try {
-                Jsoup.parse(File(filePath).readText(), "", Parser.xmlParser())!!
-            } catch (e: FileNotFoundException) {
-                display.background = Color.BLACK
-                display.title = e.message
-                println("Unable to open file '$fileName'")
-                return@addEntryListener
-            } catch (e: IOException) {
-                display.background = Color.BLACK
-                display.title = e.message
-                println("Error reading file '$fileName'")
-                return@addEntryListener
-            }
-
             // 2 - generate the profiles
-            val trajectories = try {
-                val trajectoryElement = doc.getElementsByTag("Trajectory").first()
-
-                val timeStep = trajectoryElement.attr("dt").toDouble()
-                val velocity = trajectoryElement.attr("velocity").toDouble()
-                val acceleration = trajectoryElement.attr("acceleration").toDouble()
-                val jerk = trajectoryElement.attr("jerk").toDouble()
-                val wheelBaseW = trajectoryElement.attr("wheelBaseW").toDouble()
-
-                val fitMethod = FitMethod.valueOf(trajectoryElement.attr("fitMethod"))
-
-                val waypoints = trajectoryElement.getElementsByTag("Waypoint").map { waypointElement ->
-                    val xText = waypointElement.getElementsByTag("X").first().text().toDouble()
-                    val yText = waypointElement.getElementsByTag("Y").first().text().toDouble()
-                    val angleText = waypointElement.getElementsByTag("Angle").first().text().toDouble()
-
-                    Waypoint(xText, yText, angleText)
-                }.toTypedArray()
-
-                val config = Trajectory.Config(fitMethod, Trajectory.Config.SAMPLES_HIGH, timeStep, velocity, acceleration, jerk)
-                val trajectory = Pathfinder.generate(waypoints, config)
-                val modifier = TankModifier(trajectory)
-                modifier.modify(wheelBaseW)
-
-                arrayOf(modifier.leftTrajectory, modifier.rightTrajectory)
-            } catch (e: Exception) {
-                display.background = Color.BLACK
-                display.title = e.message
-                println("Could not generate trajectory from waypoints in '$fileName'")
-                return@addEntryListener
-            }
+            val trajectories = generatePath(folderName, fileName)
 
             // serialize and publish the responses
             try {
@@ -96,13 +50,63 @@ object PathFeeder {
 
             println("Processed request: $folderName/$fileName")
             display.background = Color.GREEN
-        }, EntryListenerFlags.kImmediate)
+        }, EntryListenerFlags.kNew or EntryListenerFlags.kUpdate)
 
         NetworkTableInstance.getDefault().addConnectionListener({
             display.background = if (it.connected) Color.GREEN else Color.RED
         }, true)
 
         pathfinderInputTable.keys.forEach { pathfinderInputTable.delete(it) }
+    }
+
+    fun generatePath(folderName: String, fileName: String): Array<Trajectory>? {
+        val filePath = "$folderName/$fileName.xml"
+
+        val doc = try {
+            Jsoup.parse(File(filePath).readText(), "", Parser.xmlParser())!!
+        } catch (e: FileNotFoundException) {
+            display.background = Color.BLACK
+            display.title = e.message
+            println("Unable to open file '$fileName'")
+            return null
+        } catch (e: IOException) {
+            display.background = Color.BLACK
+            display.title = e.message
+            println("Error reading file '$fileName'")
+            return null
+        }
+
+        try {
+            val trajectoryElement = doc.getElementsByTag("Trajectory").first()
+
+            val timeStep = trajectoryElement.attr("dt").toDouble()
+            val velocity = trajectoryElement.attr("velocity").toDouble()
+            val acceleration = trajectoryElement.attr("acceleration").toDouble()
+            val jerk = trajectoryElement.attr("jerk").toDouble()
+            val wheelBaseW = trajectoryElement.attr("wheelBaseW").toDouble()
+
+            val fitMethod = FitMethod.valueOf(trajectoryElement.attr("fitMethod"))
+
+            val waypoints = trajectoryElement.getElementsByTag("Waypoint").map { waypointElement ->
+                val xText = waypointElement.getElementsByTag("X").first().text().toDouble()
+                val yText = waypointElement.getElementsByTag("Y").first().text().toDouble()
+                val angleText = waypointElement.getElementsByTag("Angle").first().text().toDouble()
+
+                Waypoint(xText, yText, angleText)
+            }.toTypedArray()
+
+            val config = Trajectory.Config(fitMethod, Trajectory.Config.SAMPLES_HIGH, timeStep, velocity, acceleration, jerk)
+            val trajectory = Pathfinder.generate(waypoints, config)
+            val modifier = TankModifier(trajectory)
+            modifier.modify(wheelBaseW)
+
+            return arrayOf(modifier.leftTrajectory, modifier.rightTrajectory)
+        } catch (e: Exception) {
+            display.background = Color.BLACK
+            display.title = e.message
+            println("Could not generate trajectory from waypoints in '$fileName'")
+        }
+        return null
     }
 
     class PathRequest(val folderName: String, val fileName: String)
